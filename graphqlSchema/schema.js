52e0c14ddd6a02fcca91151531f91e1c1e65ad9d
Aaron Angle <acangle10@gmail.com>
2020-02-20 23:27:58 -0700

    ignore node modules

--- graphqlSchema/schema.js
+++ graphqlSchema/schema.js
@@ -1,6 +1,7 @@
 const graphql = require("graphql");
+const axios = require("axios");
 
-const { GraphQLObjectType, GraphQLArray, GraphQLInt, GraphQLString, GraphQLFloat } = graphql;
+const { GraphQLObjectType, GraphQLArray, GraphQLInt, GraphQLString, GraphQLFloat, GraphQLList, GraphQLSchema } = graphql;
 
 const Price = new GraphQLObjectType({
     name: "Price",
@@ -10,4 +11,19 @@ const Price = new GraphQLObjectType({
         l: { type: GraphQLFloat },
         o: { type: GraphQLFloat }
     })
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: "RootQueryType",
+    fields: {
+        quote: {
+            type: new GraphQLList({ Price }),
+            resolve(parent, args) {
+                return axios.get("'https://finnhub.io/api/v1/quote?symbol=AAPL&token=bogh4o7rh5rej5i71mg0'")
+                    .then(res => res.data)
+            }
+        }
+    }
 })
+
+module.exports = new GraphQLSchema()
